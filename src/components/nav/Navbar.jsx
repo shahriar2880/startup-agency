950be4f41aa80baf6897b03ac93a40de3756e1f5
shahriar2880 <shahriarreza436@gmail.com>
2025-04-24 03:43:12 +0600

    create the nav elements

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Nav bar
-    </div>
+    <nav className='fixed top-0 left-0 right-0 bg-white/90 shadow-sm backdrop-blur-sm z-50 border-b border-gray-200'>
+      <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16'>
+        {/* logo */}
+        {/* desktop navitems */}
+        {/* get in touch */}
+      </div>
+    </nav>
   )
 }
 

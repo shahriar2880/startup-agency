ee750da60524e826031dc1d95fc3f7170af4723a
shahriar2880 <shahriarreza436@gmail.com>
2025-04-25 00:07:18 +0600

    navlinks

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const Navbar = () => {
+
+  const navlinks = [
+    { href: '#Home', label: 'Home' },
+    { href: '#About', label: 'About Us' },
+    { href: '#Services', label: 'Services' },
+    { href: '#Contact', label: 'Contact' },
+    { href: '#testimonials', label: 'Testimonials' },
+  ]
+
   return (
     <nav className='fixed top-0 left-0 right-0 bg-white/90 shadow-sm backdrop-blur-sm z-50 border-b border-gray-200'>
       <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16'>

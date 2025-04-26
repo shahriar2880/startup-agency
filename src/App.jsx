8c07540a2f6d2999c99f5041f71db206f29ef908
shahriar2880 <shahriarreza436@gmail.com>
2025-04-27 01:27:05 +0600

    create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Hero from './components/hero/Hero'
 import Navbar from './components/nav/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
     </>
   )
 }

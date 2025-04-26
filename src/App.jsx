2a8c6d0299525da8d1cda1082dd6cc1ec0dadb52
shahriar2880 <shahriarreza436@gmail.com>
2025-04-27 03:06:56 +0600

    add gradient color in bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import Hero from './components/hero/Hero'
-import Navbar from './components/nav/Navbar'
+import "./App.css";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/nav/Navbar";
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
-      <Hero/>
-    </>
-  )
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-bl from-indigo-300/30 to-blue-200/30 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;

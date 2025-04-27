dd04c4d45ad03d333e8c3f38e194355bd70ee04b
shahriar2880 <shahriarreza436@gmail.com>
2025-04-28 04:54:40 +0600

    complete purpose section

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import PurposeSection from "./components/purpose/PurposeSection";
 function App() {
   return (
     <main className="relative min-h-screen overflow-x-hidden">
-      <div className="absolute -top-28 -right-28 w-[500px] h-[500px] bg-gradient-to-r from-indigo-500/10 to-blue-500/10 rounded-full blur-[80px] -z-10"></div>
-      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-l from-indigo-600/10 to-blue-600/10 rounded-full blur-[80px] -z-10"></div>
+      <div className="absolute -top-28 -right-28 w-[500px] h-[500px] bg-gradient-to-r from-indigo-500/30 to-red-500/40 rounded-full blur-[80px] -z-10"></div>
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-l from-indigo-500/30 to-red-500/40 rounded-full blur-[80px] -z-10"></div>
       <div className="overflow-hidden">
         <Navbar />
         <Hero />

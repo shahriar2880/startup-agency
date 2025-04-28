84b5a4558031ee11fe430bef4df8619c4090b7fc
shahriar2880 <shahriarreza436@gmail.com>
2025-04-28 20:36:36 +0600

    complete features section

--- src/components/features/FeaturesSection.jsx
+++ src/components/features/FeaturesSection.jsx
@@ -40,7 +40,7 @@ const FeaturesSection = () => {
             >
               <div className="text-3xl">{feature.icon}</div>
             </div>
-            <h3 className="text-2xl font-medium mb-3">{feature.title}</h3>
+            <h3 className="text-2xl font-medium text-center mb-3">{feature.title}</h3>
             <p className="text-gray-500 text-center">{feature.description}</p>
           </div>
         ))}
@@ -48,7 +48,7 @@ const FeaturesSection = () => {
       <div className="text-center mt-12">
         <button className="bg-blue-600 text-white cursor-pointer px-8 py-3 rounded-full font-medium hover:bg-blue-700 transition-colors relative">
           Become a Partner
-          <div className="absolute -z-10 w-full h-full rounded-full bg-blue-800/40 blur-xl top-0 left-0"></div>
+          <div className="absolute -z-10 w-full h-full rounded-full bg-blue-800/40 blur-lg top-0 left-0"></div>
         </button>
       </div>
     </section>

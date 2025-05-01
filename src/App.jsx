88aa4522def29b4a3dbae37b33345d1749d2aa3a
shahriar2880 <shahriarreza436@gmail.com>
2025-05-02 02:37:13 +0600

    complete services section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from "./components/nav/Navbar";
 import PricingSection from "./components/pricingsection/PricingSection";
 import PurposeSection from "./components/purpose/PurposeSection";
 import ScheduleSection from "./components/schedulesection/ScheduleSection";
+import ServicesSection from "./components/services/ServicesSection";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <ScheduleSection/>
         <MonitorSection/>
         <PricingSection/>
+        <ServicesSection/>
       </div>
     </main>
   );
